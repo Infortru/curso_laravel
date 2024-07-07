a74d5f00f43291993b165938a5930ef603b72eb1
Infortru <160344951+Infortru@users.noreply.github.com>
2024-07-07 20:02:35 +0200

    Controladores

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        return "Aquí se mostrarán todos los posts";
+    }
+    public function create(){
+        return "Aquí se mostrará un formulario para crear un post";
+    }
+    public function show($post){
+        return "Aquí se muestra el post de {$post}";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get("/posts/create", [PostController::class, 'create']);
+
+Route::get("/posts/{post}", [PostController::class, 'show']);
+
+
+Route::get("/posts/{post}/{category?}", function ($post, $category=null){       //Ruta con contenido variable, entre llaves definimos la ruta
+                                                                                //variable y le pasamos la ruta a la función. El segundo
+        if($category){                                                          //parámetro es opcional y hay que asignarle el valor "null"
+            return "Aquí se muestra el post de {$post} de la categoría {$category}";
+        }else{
+            return "Aquí se muestra el post de {$post}";
+        }                                                                         
+    
 });
+
+/************Tipos de peticiones******************/
+
+//get -> A través de la URL
+//post -> A través de un formulario, oculta los datos
+//put -> Tipo post para actualizar algún registro
+//patch -> Tipo post para actualizar algún registro
+//delete -> Tipo post para borrar algún registro
